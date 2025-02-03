dea34811009fceaabd6b8009d5d293ff8cf0e376
Aaron VerDow <aaron@verdow.com>
2025-02-02 20:00:38 -0600

    allow export of package versions

--- flake.nix
+++ flake.nix
@@ -9,20 +9,28 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      packages = with pkgs; [
+        mupdf.dev
+        harfbuzz.dev
+        freetype.dev
+        jbig2dec
+        libjpeg.dev
+        openjpeg.dev
+        gumbo
+        mujs
+        zlib.dev
+        zig
+      ];
     in {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          mupdf.dev
-          harfbuzz.dev
-          freetype.dev
-          jbig2dec
-          libjpeg.dev
-          openjpeg.dev
-          gumbo
-          mujs
-          zlib.dev
-          zig
-        ];
+        buildInputs = packages;
       };
+
+      # Expose package versions as an output
+      # nix eval .#packageVersions --json | jq
+      packageVersions = builtins.listToAttrs (map (pkg: {
+        name = pkg.pname or pkg.name;
+        value = pkg.version;
+      }) packages);
     };
 }
